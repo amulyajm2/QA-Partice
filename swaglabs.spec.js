5e69647df6f78bb73c04cc6ae6dc97989bcd3544
amulyajm2 <amulyajm2@gmail.com>
2025-11-12 15:51:44 +0530

    Add files via upload

---
+++ swaglabs.spec.js
@@ -0,0 +1,66 @@
+import { test, expect } from '@playwright/test';
+import login from "../testdata/login.json";
+test(' Valid login - standard_user', async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/v1/index.html")
+    await page.locator("input[data-test='username']").fill('standard_user')
+    await page.locator("input[data-test='password']").fill('secret_sauce')
+    await page.locator("input[type='submit']").click()
+    await expect(page.locator('#inventory_container').nth(1)).toBeVisible();
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+});
+test(' Valid login - locked_out_user', async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/v1/index.html")
+    await page.locator("input[data-test='username']").fill('locked_out_user')
+    await page.locator("input[data-test='password']").fill('secret_sauce')
+    await page.locator("input[type='submit']").click()
+    await expect(page.locator("//h3[@data-test='error']")).toBeVisible()
+    await expect(page).toHaveURL('https://www.saucedemo.com/')
+});
+test(' Valid login - problem_user', async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/v1/index.html")
+    await page.locator("input[data-test='username']").fill('problem_user')
+    await page.locator("input[data-test='password']").fill('secret_sauce')
+    await page.locator("input[type='submit']").click()
+    await expect(page.locator('#inventory_container').nth(1)).toBeVisible();
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+});
+test(' Valid login - performance_glitch_user', async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/v1/index.html")
+    await page.locator("input[data-test='username']").fill('performance_glitch_user')
+    await page.locator("input[data-test='password']").fill('secret_sauce')
+    await page.locator("input[type='submit']").click()
+    await expect(page.locator('#inventory_container').nth(1)).toBeVisible();
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+});
+test(' invalid_username - wrong_user', async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/v1/index.html")
+    await page.locator("input[data-test='username']").fill('wrong_user')
+    await page.locator("input[data-test='password']").fill('secret_sauce')
+    await page.locator("input[type='submit']").click()
+    await expect(page.locator("h3[data-test='error']")).toBeVisible();
+    await expect(page).toHaveURL('https://www.saucedemo.com/')
+});
+test(' invalid_password - wrong_password', async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/v1/index.html")
+    await page.locator("input[data-test='username']").fill('standard_user')
+    await page.locator("input[data-test='password']").fill('wrong_password')
+    await page.locator("input[type='submit']").click()
+    await expect(page.locator("h3[data-test='error']")).toBeVisible();
+    await expect(page).toHaveURL('https://www.saucedemo.com/')
+});
+test(' empty_username -  ', async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/v1/index.html")
+    await page.locator("input[data-test='username']").fill(' ')
+    await page.locator("input[data-test='password']").fill('wrong_password')
+    await page.locator("input[type='submit']").click()
+    await expect(page.locator("h3[data-test='error']")).toBeVisible();
+    await expect(page).toHaveURL('https://www.saucedemo.com/')
+});
+test(' empty_password -  ', async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/v1/index.html")
+    await page.locator("input[data-test='username']").fill(' standard_user')
+    await page.locator("input[data-test='password']").fill(' ')
+    await page.locator("input[type='submit']").click()
+    await expect(page.locator("h3[data-test='error']")).toBeVisible();
+    await expect(page).toHaveURL('https://www.saucedemo.com/')
+});
